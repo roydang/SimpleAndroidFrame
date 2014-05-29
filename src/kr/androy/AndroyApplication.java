0d71438197ef02ae4c20737aba74e955a2674755
HyongSoon Choi <roydang@gmail.com>
2014-05-29 16:13:37 +0900

    프로젝트 분리

--- src/kr/androy/AndroyApplication.java
+++ src/kr/androy/AndroyApplication.java
@@ -4,5 +4,20 @@ import android.app.Application;
 
 public class AndroyApplication extends Application {
 
+	public static AndroyApplication _internalInstance;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		_internalInstance = this;
+		
+		//http://code.google.com/p/android/issues/detail?id=20915
+		try {
+			Class.forName("android.os.AsyncTask");
+		} catch (ClassNotFoundException e) {
+		}
+		
+		
+	}
 
 }
---
+++ src/kr/androy/base/config/AppBuildCheckFlag.java
@@ -0,0 +1,10 @@
+package kr.androy.base.config;
+
+
+public class AppBuildCheckFlag {
+
+
+	public static boolean DEBUG_MODE = true;
+
+	
+}
